7e7954838bd053db38af0fb8daad0772d86714e2
LoreLor <lorenadearmas01@gmail.com>
2022-02-02 21:01:38 -0300

    My Task List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, EmpleadosComponent, EmpleadoComponent, HeaderComponent
   ],
   imports: [
     BrowserModule
