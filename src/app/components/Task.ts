4a6fb4458e6d8f218455c99f4b575eff6fd5c97f
LoreLor <lorenadearmas01@gmail.com>
2022-02-02 21:50:45 -0300

    agregue archivos Task

---
+++ src/app/components/Task.ts
@@ -0,0 +1,6 @@
+export interface Task {
+    id?: number,  //id opcional, puede venir como no
+    text: string,
+    day: string,
+    reminder: boolean,
+}
---
+++ src/app/components/mock-task.ts
@@ -0,0 +1,29 @@
+import { Task } from "./Task"
+
+export const TASK: Task[] =[
+    {
+        id:1,
+        text:"Terminar primer modulo de angular",
+        day: 'Febrero 10 a las 12:00',
+        reminder:true
+    },
+    {
+        id:2,
+        text:"HAcer las compras",
+        day: 'Febrero 10 a las 14:00',
+        reminder:true
+    },
+    {
+        id:3,
+        text:"Investigar sobre Boostrapt",
+        day: 'Febrero 10 a las 17:00',
+        reminder:false
+    },
+    {
+        id:4,
+        text:"Ir al Club",
+        day: 'Febrero 10 a las 20:00',
+        reminder:false
+    },
+
+]
