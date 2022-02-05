707270873c6706da16e67ed3ff8938e25189521e
LoreLor <lorenadearmas01@gmail.com>
2022-02-05 19:08:52 -0300

    funcionalidad de manejo de from terminada

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../service/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
   title:string= "My Task List";
+  showAddTask:boolean = false;
+  subscription?: Subscription;
 
-  constructor() { }
+  constructor(
+    private uiService: UiService
+  ) {
+    this.subscription = this.uiService.onToggle()
+      .subscribe(value => this.showAddTask = value)
+   }
 
   ngOnInit(): void {
   }
 
   toogleAddTask(){
-    console.log('toogleAdd')
+    //console.log('toogleAdd')
+    this.uiService.toogleAddTask();
   }
 
 }
